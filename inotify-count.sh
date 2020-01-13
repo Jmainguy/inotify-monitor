e2d9509c227f4a538690d25da5549c66641fd9b9
Jonathan Mainguy <jon@soh.re>
2020-01-13 12:18:59 -0500

    fix logic

--- inotify-count.sh
+++ inotify-count.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 # Credit for the line= logic from https://raw.githubusercontent.com/fatso83/dotfiles/master/utils/scripts/inotify-consumers
 
-lines=$(
-    find /opt/proc/*/fd \
-    -lname anon_inode:inotify \
-    -printf '%hinfo/%f\n' 2>/dev/null \
-    \
-    | xargs grep -c '^inotify'  \
-    | sort -n -t: -k2 -r -u \
-    )
-
 while true; do
 
     count=0
+    lines=$(
+        find /opt/proc/*/fd \
+        -lname anon_inode:inotify \
+        -printf '%hinfo/%f\n' 2>/dev/null \
+        \
+        | xargs grep -c '^inotify'  \
+        | sort -n -t: -k2 -r -u \
+    )
+
     for line in $lines; do
         watcher_count=$(echo $line | sed -e 's/.*://')
         count=$(($count + $watcher_count))
